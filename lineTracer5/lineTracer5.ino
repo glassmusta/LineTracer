b177eefa5eeb3aecb5bfaad9d0a0277abaaed916
glassmusta <ourclub7279@gmail.com>
2016-12-01 11:59:00 +0900

    dd

--- lineTracer5/lineTracer5.ino
+++ lineTracer5/lineTracer5.ino
@@ -4,8 +4,8 @@
 #define E2 5
 
 int pins[4] = {9,10,11,12};
+bool readPins[4];
 //int sensorRef[4] = {600,600,600,600};
-int readPins[4];
 bool status = false;
 bool swStatus = true;
 
@@ -13,6 +13,7 @@ void setup() {
   // put your setup code here, to run once:
   for(int i=2;i<6;++i) pinMode(i,OUTPUT);
   for(int i=0;i<4;++i) pinMode(pins[i],INPUT);
+  Serial.begin(9600);
   pinMode(8,INPUT);
   digitalWrite(M2,HIGH);
   digitalWrite(M1,LOW);
@@ -20,9 +21,8 @@ void setup() {
 
 void loop() {
   for(int i=0;i<4;++i){
-    readPins[i] = analogRead(pins[i]);
+    readPins[i] = digitalRead(pins[i]);
   }
-
   if(digitalRead(8) == LOW && swStatus == true){
     status = !status;
     swStatus = false;
@@ -30,46 +30,48 @@ void loop() {
     swStatus = true;
   }
   if(status){
-     if(digitalRead(readPins[0])==HIGH && digitalRead(readPins[1])==HIGH && digitalRead(readPins[2])==HIGH && digitalRead(readPins[3])==HIGH) 
-    {
+    if(readPins[0] ==LOW && readPins[1] == LOW && readPins[2] == LOW && readPins[3] ==LOW){
       goFoward();
-    }else if(digitalRead(readPins[0]) ==LOW && digitalRead(readPins[1]) == LOW && digitalRead(readPins[2]) == LOW && digitalRead(readPins[3]) ==LOW){
+      Serial.println("1111");
+    }
+    else if(readPins[0]==LOW && readPins[1]==LOW || readPins[2] ==LOW)
+    {
+      Serial.println("1110");
+      turn(80,20);      
+    }
+    else if(readPins[2]==LOW && readPins[3]==LOW || readPins[1] ==LOW)
+    {
+      Serial.println("0111");
+      turn(20,80);
+    }
+     else if(readPins[1] ==LOW && readPins[2]==LOW)
+    {
+      Serial.println("0110");
       goFoward();
-    }else if(digitalRead(readPins[0])==LOW && digitalRead(readPins[1])==LOW && digitalRead(readPins[2])==LOW){
-      turn(10,60); 
-    }else if(digitalRead(readPins[1])==LOW && digitalRead(readPins[2])==LOW && digitalRead(readPins[3])==LOW){
-      turn(60,10);
     }
-    else if(digitalRead(readPins[1]) ==LOW && digitalRead(readPins[2])==LOW)
+    else if(readPins[0]==LOW)  // 맨 오른쪽 센서(A0)에 라인이 감지된 경우
     {
-      goFoward();
+      Serial.println("1000");
+      turn(20,80);
     }
-    else if(digitalRead(readPins[0])==LOW && digitalRead(readPins[1])==LOW)
+    else if(readPins[3]==LOW)  // 맨 왼쪽 센서(A5)에 라인이 감지된 경우
     {
-      turn(60,10);      
+      Serial.println("0001");
+      turn(80,20);
     }
-    else if(digitalRead(readPins[2])==LOW && digitalRead(readPins[3])==LOW)
+    else if(readPins[1]==LOW)  // 오른쪽에서 두 번째 센서(A1)에 라인이 감지된 경우
     {
-      turn(10,60);
+      Serial.println("0100");
+      turn(30,80); 
     }
-    else if(digitalRead(readPins[0])==LOW)  // 맨 오른쪽 센서(A0)에 라인이 감지된 경우
+    else if(readPins[2]==LOW)  // 왼쪽에서 두 번째 센서(A4)에 라인이 감지된 경우
     {
-      turn(10,60);
-    }
-    else if(digitalRead(readPins[3])==LOW)  // 맨 왼쪽 센서(A5)에 라인이 감지된 경우
-    {
-      turn(60,10);
-    }
-    else if(digitalRead(readPins[1])==LOW)  // 오른쪽에서 두 번째 센서(A1)에 라인이 감지된 경우
-    {
-      turn(25,55); 
-    }
-    else if(digitalRead(readPins[2])==LOW)  // 왼쪽에서 두 번째 센서(A4)에 라인이 감지된 경우
-    {
-      turn(55,25);
+      Serial.println("0010");
+      turn(80,30);
     }
     else{
-      setup();
+      Serial.println("0000");
+      goFoward();
     }
   }else if(status == false){
     Stop();
@@ -84,8 +86,8 @@ void Stop(){
     digitalWrite(E1,LOW);
 }
 void goFoward(){
-    analogWrite(E1,80);
-    analogWrite(E2,80);
+    analogWrite(E1,120);
+    analogWrite(E2,120);
 }
 void turn(int l,int r){
     analogWrite(E1,l);
